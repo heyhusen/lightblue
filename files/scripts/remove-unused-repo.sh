b6382416919ea5cdbd0958ba52a2c85cf55c28ca
Husen <hi@husen.id>
2026-01-25 10:24:55 +0700

    build: add antigravity
    Signed-off-by: Husen <hi@husen.id>

--- files/scripts/remove-unused-repo.sh
+++ files/scripts/remove-unused-repo.sh
@@ -12,3 +12,4 @@ dnf -y copr remove dejan/lazygit
 dnf -y copr remove lihaohong/yazi
 dnf -y copr remove rivenirvana/lazydocker
 rm /etc/yum.repos.d/vscode.repo
+rm /etc/yum.repos.d/antigravity.repo
